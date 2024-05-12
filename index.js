3e11a539053c82b495f4f99a797fa123b9cd5caa
Aayush Regmi <aayu.regmi10@gmail.com>
2024-05-12 09:18:43 +0545

    Add typecheck and tipcalculation functions

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-let tip=null
-let billAmount=null
-let tipRate=null
+let tip = null;
+let billAmount = null;
+let tipRate = null;
 
-const promptValues=()=>{
-    billAmount=prompt('Enter bill amount: ')
-    tipRate=prompt('Enter tip rate: ')
-}
+const promptValues = () => {
+  billAmount = prompt("Enter bill amount: ");
+  tipRate = prompt("Enter tip rate: ");
+};
+
+const calculateTip = (billAmount, tipRate) => {
+  return Math.round(billAmount * (tipRate / 100));
+};
+
+const typeCheck = (billAmount, tipRate) => {
+  if (
+    isNaN(billAmount) ||
+    isNaN(tipRate) ||
+    billAmount === "" ||
+    billAmount === " " ||
+    tipRate === "" ||
+    tipRate === " " ||
+    billAmount < 0 ||
+    tipRate < 0
+  ) {
+    console.log("Please Enter Valid Numbers!");
+    return false;
+  } else {
+    return true;
+  }
+};
